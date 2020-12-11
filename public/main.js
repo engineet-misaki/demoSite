aa884256a3caae1d1a362fac24597de5c1a1fc35
misaki toyama <misakitoyama@misakinoMacBook-Pro.local>
2020-12-11 09:33:36 +0900

    first commit

---
+++ public/main.js
@@ -0,0 +1,46 @@
+
+ScrollReveal().reveal('.fadeContets', {
+    duration: 1000, // アニメーションの完了にかかる時間
+    viewFactor: 0.4, // 0~1,どれくらい見えたら実行するか
+    reset: true,   // 何回もアニメーション表示するか
+    // distance: "1000px",
+    scale: 0.1,
+});
+ScrollReveal().reveal('.slideContets', {
+    duration: 1000, // アニメーションの完了にかかる時間
+    viewFactor: 0.4, // 0~1,どれくらい見えたら実行するか
+    reset: true,   // 何回もアニメーション表示するか
+    distance: "1000px",
+    origin: "left"
+    // scale: 0.1,
+});
+ScrollReveal().reveal('.rotateContets', {
+    duration: 1000, // アニメーションの完了にかかる時間
+    viewFactor: 0.4, // 0~1,どれくらい見えたら実行するか
+    reset: true,   // 何回もアニメーション表示するか
+    rotate: {x:90, z: 90}
+    // distance: "1000px",
+    // scale: 0.1,
+});
+ScrollReveal().reveal('.card1', {
+    duration: 2000, // アニメーションの完了にかかる時間
+    reset: true,   // 何回もアニメーション表示するか
+    distance: "1000px",
+    origin: "right"
+});
+ScrollReveal().reveal('.card2', {
+    duration: 2000, // アニメーションの完了にかかる時間
+    reset: true,   // 何回もアニメーション表示するか
+    distance: "1000px",
+    origin: "right"
+});
+ScrollReveal().reveal('.card3', {
+    duration: 2000, // アニメーションの完了にかかる時間
+    reset: true,   // 何回もアニメーション表示するか
+    distance: "1000px",
+    origin: "right"
+});
+
+
+
+
