8e3b7008ac15f8d93784eadc5ededfcfa3ff11bb
misaki toyama <misakitoyama@misakinoMacBook-Pro.local>
2021-05-12 08:18:52 +0900

    自己紹介用に編集

--- public/main.js
+++ public/main.js
@@ -8,38 +8,38 @@ ScrollReveal().reveal('.fadeContets', {
 });
 ScrollReveal().reveal('.slideContets', {
     duration: 1000, // アニメーションの完了にかかる時間
-    viewFactor: 0.4, // 0~1,どれくらい見えたら実行するか
-    reset: true,   // 何回もアニメーション表示するか
+    viewFactor: 0.1, // 0~1,どれくらい見えたら実行するか
+    reset: false,   // 何回もアニメーション表示するか
     distance: "1000px",
     origin: "left"
     // scale: 0.1,
 });
-ScrollReveal().reveal('.rotateContets', {
-    duration: 1000, // アニメーションの完了にかかる時間
-    viewFactor: 0.4, // 0~1,どれくらい見えたら実行するか
-    reset: true,   // 何回もアニメーション表示するか
-    rotate: {x:90, z: 90}
-    // distance: "1000px",
-    // scale: 0.1,
-});
-ScrollReveal().reveal('.card1', {
+// ScrollReveal().reveal('.rotateContets', {
+//     duration: 1000, // アニメーションの完了にかかる時間
+//     viewFactor: 0.4, // 0~1,どれくらい見えたら実行するか
+//     reset: true,   // 何回もアニメーション表示するか
+//     rotate: {x:90, z: 90}
+//     // distance: "1000px",
+//     // scale: 0.1,
+// });
+ScrollReveal().reveal('.card', {
     duration: 2000, // アニメーションの完了にかかる時間
-    reset: true,   // 何回もアニメーション表示するか
+    reset: false,   // 何回もアニメーション表示するか
     distance: "1000px",
     origin: "right"
 });
-ScrollReveal().reveal('.card2', {
-    duration: 2000, // アニメーションの完了にかかる時間
-    reset: true,   // 何回もアニメーション表示するか
-    distance: "1000px",
-    origin: "right"
-});
-ScrollReveal().reveal('.card3', {
-    duration: 2000, // アニメーションの完了にかかる時間
-    reset: true,   // 何回もアニメーション表示するか
-    distance: "1000px",
-    origin: "right"
-});
+// ScrollReveal().reveal('.card2', {
+//     duration: 2000, // アニメーションの完了にかかる時間
+//     reset: true,   // 何回もアニメーション表示するか
+//     distance: "1000px",
+//     origin: "right"
+// });
+// ScrollReveal().reveal('.card3', {
+//     duration: 2000, // アニメーションの完了にかかる時間
+//     reset: true,   // 何回もアニメーション表示するか
+//     distance: "1000px",
+//     origin: "right"
+// });
 
 
 
